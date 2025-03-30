76b678b680bf12db1be8d323df0105e9d7d4ef9e
Kenneth Kiptenten <tentenkenneth@outlook.com>
2025-03-30 14:00:14 +0300

    update config.js

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://kip1010:admin123@ip1.x22oalz.mongodb.net/?retryWrites=true&w=majority&appName=IP1',
-    development: 'mongodb+srv://kip1010:admin123@ip1.x22oalz.mongodb.net/?retryWrites=true&w=majority&appName=IP1',
+    development: 'mongodb+srv://kip101:admin123@ip1.x22oalz.mongodb.net/?retryWrites=true&w=majority&appName=IP1',
     test: 'mongodb+srv://kip1010:admin123@ip1.x22oalz.mongodb.net/?retryWrites=true&w=majority&appName=IP1',
 }
 module.exports = config;
